7f3eebf5acaacc9c4c77d7c7ce6c9c3d731d53b3
raskilo <raskilo@live.com.pt>
2022-06-30 11:13:04 +0100

    Characters maximum health, and ViewCharacter Stats

--- Character.js
+++ Character.js
@@ -1,60 +1,70 @@
-function Character(name, profession, gold, health, attack, level, xp, nextLevelAt) {
+function Character(name, profession, gold, health, maxHealth, attack, level, xp, nextLevelAt) {
     this.name = name;
     this.gold = gold || 100;
     this.level = level || 1;
     this.xp = xp || 0;
     this.nextLevelAt = nextLevelAt || 100;
-    
+
 
     switch (profession) {
         case 'Warrior':
             this.profession = new WarriorBehavior(health, attack);
+            this.maxHealth = maxHealth || 100;
+            this.health = health || 100;
             break;
         case 'Thief':
             this.profession = new ThiefBehavior(health, attack);
+            this.maxHealth = maxHealth || 75;
+            this.health = health || 75;
             break;
         case 'Mage':
             this.profession = new MageBehavior(health, attack);
+            this.maxHealth = maxHealth || 50;
+            this.health = health || 50;
             break;
         default:
             throw new Error(`${profession} is not an implemented class`);
     }
+
+
 }
 
-Character.prototype.doDamage = function(enemy) {
+Character.prototype.doDamage = function (enemy) {
     // Delegate the doDamage method to it's specialized profession doDamage
     this.profession.doDamage(enemy)
 }
 
-Character.prototype.receiveDamage = function(amount) {
-    this.profession.health -= amount;
+Character.prototype.receiveDamage = function (amount) {
+    this.health -= amount;
 }
 
-Character.prototype.gainXP = function(xp) {
-    const multiplier = Math.min(1 + this.level/10, 3);
+Character.prototype.gainXP = function (xp) {
+    const multiplier = Math.min(1 + this.level / 10, 3);
     const xpGained = ~~(xp * multiplier);
     this.xp += xpGained;
 }
 
-Character.prototype.levelUp = function() {
+Character.prototype.levelUp = function () {
     this.level += 1;
     this.profession.attack += 2;
-    addedHealth =  (this.level - 1) * 20;
+    addedHealth = (this.level - 1) * 20;
 
-    switch (this.profession.profession) {
-        case "Warrior":
-            this.profession.health = 100 + addedHealth;
-            break;
-        case "Thief":
-            this.profession.health = 75 + addedHealth;
-            break;
-        case "Mage":
-            this.profession.health = 50 + addedHealth;
-            break;
-        default: console.log("You health was not increased");
-    }
+    this.maxHealth += addedHealth;
+    this.maxHealth += addedHealth;
 
     this.nextLevelAt = Math.floor(this.nextLevelAt + (this.nextLevelAt * 2) - (this.level * 50));
+}
+
+// Creation of an object that represents the stats of the character
+Character.prototype.viewCharacter = function () {
+    return {
+        'Name': this.name,
+        'Gold': this.gold,
+        'XP': this.xp + '/' + this.nextLevelAt,
+        'Health': this.health + '/' + this.maxHealth,
+        'Attack': this.profession.attack,
+        'Profession': this.profession.profession
+    };
 }
 
 /**
@@ -62,12 +72,12 @@ Character.prototype.levelUp = function() {
  */
 
 function WarriorBehavior(health, attack) {
-    this.health = health || 100;
+    this.baseHealth = health || 100;
     this.attack = attack || 10;
     this.profession = "Warrior";
 }
 
-WarriorBehavior.prototype.doDamage = function(enemy) {
+WarriorBehavior.prototype.doDamage = function (enemy) {
     enemy.health -= this.attack;
 }
 
@@ -77,14 +87,14 @@ WarriorBehavior.prototype.doDamage = function(enemy) {
  */
 
 function ThiefBehavior(health, attack) {
-    this.health = health || 75;
+    this.baseHealth = health || 75;
     this.attack = attack || 10;
     this.criticalChance = 0.15
     this.criticalModifier = 3;
     this.profession = "Thief";
 }
 
-ThiefBehavior.prototype.doDamage = function(enemy) {
+ThiefBehavior.prototype.doDamage = function (enemy) {
     const damage = Math.random() <= this.criticalChance
         ? this.attack * this.criticalModifier
         : this.attack;
@@ -97,12 +107,12 @@ ThiefBehavior.prototype.doDamage = function(enemy) {
  */
 
 function MageBehavior(health, attack) {
-    this.health = health || 50;
+    this.baseHealth = health || 50;
     this.attack = attack || 15;
     this.profession = "Mage";
 }
 
-MageBehavior.prototype.doDamage = function(enemy) {
+MageBehavior.prototype.doDamage = function (enemy) {
     enemy.health -= this.attack;
 }
 
